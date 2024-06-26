eaa59f402666024723550fda6892954dd128314b
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-06-26 12:33:02 +0200

    Make following objects be pushed after combo ends

--- Look there/Assets/Scripts/Enemies/FollowingObject.cs
+++ Look there/Assets/Scripts/Enemies/FollowingObject.cs
@@ -10,6 +10,10 @@ public class FollowingObject : MonoBehaviour
     [SerializeField] float _maxSpeed;
     [SerializeField] float _force;
     [SerializeField] GameObject _objectToFollow;
+    [SerializeField] float _comboWindowTime;
+    private float _time;
+    private bool _isStillCombo = false;
+    private bool _isCombCorOn = false;
     private void Awake()
     {
         _healthSystem.OnHitEvent += Push;
@@ -17,15 +21,42 @@ public class FollowingObject : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (_isCombCorOn) return;
         transform.up = _objectToFollow.transform.position - transform.position;
         if (_rb.velocity.magnitude < _maxSpeed)
         {
             _rb.AddForce((_objectToFollow.transform.position - transform.position).normalized * _force);
         }
     }
+    IEnumerator ComboCor(DamageInfo info)
+    {
+        _isCombCorOn = true;
+        while (_time < _comboWindowTime)
+        {
+            _time += Time.deltaTime;
+            yield return null;
+            if (_isStillCombo)
+            {
+                _time = 0;
+                _isStillCombo = false;
+            }
+        }
+        _time = 0;
+        _rb.AddForce((transform.position - info.dmgPosition).normalized * _pushForce, ForceMode2D.Impulse);
+        _isCombCorOn = false;
+    }
     private void Push(DamageInfo info)
     {
-        _rb.AddForce((transform.position - info.dmgPosition).normalized * _pushForce,ForceMode2D.Impulse);
+        if (!_isCombCorOn)
+        {
+            _rb.velocity = Vector2.zero;
+            StartCoroutine(ComboCor(info));
+        }
+        else
+        {
+            _isStillCombo = true;
+        }
+        
     }
 
 }
