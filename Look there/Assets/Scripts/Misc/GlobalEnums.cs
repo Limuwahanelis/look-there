af1a7d2eb1527673d721c8137d0ad7fda06d27fc
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-04 14:14:56 +0200

    Add global enums class

---
+++ Look there/Assets/Scripts/Misc/GlobalEnums.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalEnums
+{
+    public enum HorizontalDirections
+    {
+        LEFT=-1,SAME=0,RIGHT=1
+    }
+
+}
