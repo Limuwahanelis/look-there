5a6a81986375500f044df7261309ca1abfe4f05a
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-05 15:55:09 +0200

    Move ringhandle to health system

--- Look there/Assets/Health system/Pushing/Ringhandle.cs
+++ Look there/Assets/Health system/Pushing/Ringhandle.cs
@@ -9,21 +9,15 @@ public class Ringhandle : MonoBehaviour
     private Vector3 normalized;
     private void Update()
     {
+        if (handle == null) return;
         normalized = Vector3.Normalize(handle.transform.position - transform.position);
-        Vector3 handlePos = transform.position + normalized * 2; // multiplied by 2 for better visibility
+        Vector3 handlePos = transform.position + normalized*2; // multiplied by 2 for better visibility
         handle.transform.position = handlePos;
     }
 
     public Vector3 GetVector()
     {
         return normalized / 2;
-    }
-    private void OnValidate()
-    {
-        if(handle != null) 
-        {
-
-        }
     }
 
 }
