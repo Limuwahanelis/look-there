b898ceb5ab19655788e90d8de157ac78b9b6c71e
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-05 15:53:27 +0200

    Update pushing interfaces

--- Look there/Assets/Health system/Interfaces/IPushable.cs
+++ Look there/Assets/Health system/Interfaces/IPushable.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public interface IPushable
 {
     void Push(PlayerHealthSystem.DamageType damageType, IPusher pusher = null);
-    void Push(PlayerMovement.playerDirection direction, PlayerHealthSystem.DamageType damageType);
+    void Push(Vector3 pushDirection, PlayerHealthSystem.DamageType damageType);
 
-    void Push(PlayerMovement.playerDirection direction, PlayerHealthSystem.DamageType damageType,IPusher pusher);
+    void Push(Vector3 pushDirection, PlayerHealthSystem.DamageType damageType,IPusher pusher);
 }
--- Look there/Assets/Health system/Interfaces/IPusher.cs
+++ Look there/Assets/Health system/Interfaces/IPusher.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public interface IPusher
 {
+    public Vector3 Position { get; }
     public void ResumeCollisonsWithPlayer(Collider2D[] playerCols);
     public void PreventCollisionWithPlayer(Collider2D[] playerCols);
 }
