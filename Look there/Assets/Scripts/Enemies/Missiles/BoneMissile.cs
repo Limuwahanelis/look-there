8935f631e3b4a59eafc3edcef7c6ed57b910476f
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-12 13:01:26 +0200

    Create BoneMissile.cs

---
+++ Look there/Assets/Scripts/Enemies/Missiles/BoneMissile.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneMissile : MonoBehaviour,IDamagable
+{
+    [SerializeField] Rigidbody2D _rb;
+    [SerializeField] float _rotationSpeed;
+    [SerializeField] float _speed;
+    private float _angle=0;
+    private Vector2 _direction;
+    private Vector2 _originPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void SetRotation(float startAngle)
+    {
+        _angle = startAngle;
+    }
+    public void SetDirectionAndSpeed(Vector2 target,float speed) 
+    {
+        _direction = (target - _rb.position).normalized;
+        _speed = speed;
+    }
+    private void Update()
+    {
+        _angle += _rotationSpeed * Time.deltaTime;
+        if (_angle > 360) _angle = 0;
+        _rb.rotation = _angle;
+        if (_direction == Vector2.zero) return;
+        _rb.MovePosition((_rb.position + _direction* _speed * Time.deltaTime));
+    }
+
+    public void TakeDamage(DamageInfo info)
+    {
+        _direction = -_direction;
+    }
+
+    public void Kill()
+    {
+        
+    }
+}
