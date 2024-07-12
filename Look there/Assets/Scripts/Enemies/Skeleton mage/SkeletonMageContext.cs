93427fa59e8d57bca4d03d0809cd52c30aa8ed6e
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-12 13:05:40 +0200

    Update Skeleton mage context

---
+++ Look there/Assets/Scripts/Enemies/Skeleton mage/SkeletonMageContext.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMageContext : EnemyContext
+{
+    public float playerDetectionRange;
+    public BoneMissileSpawner boneSpawner;
+    public float boneMissileCooldown;
+    public float boneSpeed;
+}
