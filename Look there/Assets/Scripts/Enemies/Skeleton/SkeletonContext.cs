d55ef1c4365822c8fe60615d6f8e1be530240866
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-07-13 17:14:06 +0200

    Update SkeletonContext.cs

--- Look there/Assets/Scripts/Enemies/Skeleton/SkeletonContext.cs
+++ Look there/Assets/Scripts/Enemies/Skeleton/SkeletonContext.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SkeletonContext : EnemyContext
 {
     public SkeletonCombat combat;
+    public EnemyMovement enemyMovement;
     public float maxPlayerDistance;
     public float minPlayerDistance;
 }
